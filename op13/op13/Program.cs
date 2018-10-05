b44d95d563b6d2dc6575609bbc081a4be0ddb9ad
Simona Colapicchioni <s.colapicchioni@gmail.com>
2018-10-05 13:01:21 +0200

    UniqueRandomNumberGenerator

---
+++ op13/op13/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace op13 {
+    class Program {
+        static void Main(string[] args) {
+            /*
+             * Write a program that fills an array 
+             * with dimension 3 x 3 
+             * with random numbers between 1 and 9. 
+             * Each random number may only occur 
+             * once in the array.
+             * */
+
+            //INGREDIENTS:
+            int rows = 3;
+            int cols = 3;
+            int[,] matrix = new int[rows, cols];
+            
+            //FILL IT:
+            fillMatrix(matrix);
+
+            //PRINT IT:
+            printMatrix(matrix);
+
+            Console.ReadLine();
+        }
+
+        private static void printMatrix(int[,] matrix) {
+            for (int row = 0; row < matrix.GetLength(0); row++) {
+                for (int col = 0; col < matrix.GetLength(1); col++) {
+                    Console.Write($"{matrix[row, col]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void fillMatrix(int[,] matrix) {
+            //INGREDIENTS:
+            int numbersInTotal = matrix.GetLength(0) * matrix.GetLength(1);
+
+            UniqueRandomNumberGenerator generator = new UniqueRandomNumberGenerator(numbersInTotal);
+            
+            //FILL IT:
+            for (int row = 0; row < matrix.GetLength(0); row++) {
+                for (int col = 0; col < matrix.GetLength(1); col++) {
+                    matrix[row, col] = generator.Next();
+                }
+            }
+        }
+
+    }
+}
---
+++ op13/op13/UniqueNumberGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace op13 {
+    public class UniqueRandomNumberGenerator {
+        //constructor
+        //I can use it like this:
+        //UniqueNumberGenerator gen = new UniqueNumberGenerator(10);
+        public UniqueRandomNumberGenerator(int max) {
+            generator = new Random();
+            alreadyExtractedNumbers = new bool[max];
+            counter = 0;
+        }
+
+        Random generator;
+        bool[] alreadyExtractedNumbers;
+        int counter;
+
+        //I can use it like this:
+        //int n = generator.Next();
+        public int Next() 
+        {
+            if (counter > alreadyExtractedNumbers.Length) {
+                throw new InvalidOperationException("I'm out of numbers, sorry....");
+            }
+
+            //keep extracting if you already have it in the list
+            int number;
+            do {
+                number = generator.Next(1, alreadyExtractedNumbers.Length + 1);
+            } while (alreadyExtractedNumbers[number - 1]);
+            //if I'm here, I have a new number 
+            alreadyExtractedNumbers[number - 1] = true;
+            counter++;
+            return number;
+        }
+    }
+}
