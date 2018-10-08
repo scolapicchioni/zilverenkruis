ad40a60364ef7c6455fc00508fe7aa3b6a89293e
Simona Colapicchioni <s.colapicchioni@gmail.com>
2018-10-08 10:49:32 +0200

    evaluation form

---
+++ op15/op15.ConsoleApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace op15.ConsoleApplication {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
