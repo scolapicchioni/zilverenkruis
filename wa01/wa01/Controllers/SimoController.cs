80b2d0f324a303785acb5180a79897352aa3410f
Simona Colapicchioni <s.colapicchioni@gmail.com>
2018-10-29 10:33:51 +0100

    Web Day 01 - ASP.NET MVC

---
+++ wa01/wa01/Controllers/SimoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wa01.Models;
+
+namespace wa01.Controllers
+{
+    public class SimoController : Controller
+    {
+        public string SayHi() {
+            return "<html><head></head><body><h1>Hello World!</h1></body></html>";
+        }
+
+        public ViewResult SayHiWithView() {
+            return View();
+        }
+
+        public ViewResult GetPerson() {
+            Person person = new Person() {Id=1, Name = "Simona", Surname="Colapicchioni", Age=44 }; 
+            return View(person);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
